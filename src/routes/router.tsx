7844927a8ac0b852beb592a038ab75b530b08b72
vitrcosta <44215161+vitrcosta@users.noreply.github.com>
2024-01-24 19:22:01 -0300

    routes

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -8,7 +8,7 @@ export const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: 'landing',
+        path: '/landing',
         element: <Landing />,
       },
       {
