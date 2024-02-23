0adeaac10ab24595db0c56680a89d399081875f1
Gustavo Tonin <gustavo.tonin@compasso.com.br>
2024-02-23 18:11:19 -0300

    Ajustes

---
+++ src/services/hook/useService.ts
@@ -0,0 +1,43 @@
+import { AxiosPromise, AxiosRequestConfig } from 'axios';
+import api from '../api';
+
+export function useService() {
+  function get(url: string, data: any, query: string): AxiosPromise {
+    return api.get(`${url}${query}`, {
+      data,
+    });
+  }
+
+  function post(url: string, data: any): AxiosPromise {
+    return api.post(`${url}`, data, {
+      headers: {},
+    });
+  }
+
+  function put(url: string, data: any): AxiosPromise {
+    return api.put(`${url}`, data, {
+      headers: {},
+    });
+  }
+
+  function patch(url: string, data: any): AxiosPromise {
+    return api.patch(`${url}`, data, {
+      headers: {},
+    });
+  }
+
+  function remove(url: string, data: any) {
+    return api.delete(`${url}`, {
+      headers: {},
+      ...data,
+    });
+  }
+
+  return {
+    get,
+    post,
+    patch,
+    put,
+    remove,
+  };
+}
